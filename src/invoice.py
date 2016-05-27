9ec45ad8fb231b272c3ccd14fe07b43669c61361
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-05-27 21:34:41 +0200

    Made the basic template a bit more useful.

--- src/invoice.py
+++ src/invoice.py
@@ -22,24 +22,23 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument('--date-of-sale', '-s', help='date of sale', default=date)
     p.add_argument('--due-date', help='payment due date', default=due)
-    p.add_argument('--iban', '-b', help='names the International Bank Account Number of the issuer', default=None)
     p.add_argument('--issuer', '-i', help='names the issuer of the invoice')
     p.add_argument('--issue-date', '-d', help='invoice issue date', default=date)
     p.add_argument('--number', '-n', help='names the number of the invoice')
     p.add_argument('--template', '-t', help='selects invoice template')
     p.add_argument('--recipient', '-r', help='names the recipient of the invoice')
     p.add_argument('ITEMS', nargs='+', help='invoice items')
+
     args = p.parse_args()
 
     generate({
         'date_of_sale': args.date_of_sale,
         'due_date': args.due_date,
-        'iban': args.iban,
         'invoice_number': args.number,
         'invoice_template': args.template,
-        'issuer': args.issuer,
+        'issuer': json.loads(args.issuer),
         'issue_date': args.issue_date,
         'items': list(map(json.loads, args.ITEMS)),
-        'recipient': args.recipient,
+        'recipient': json.loads(args.recipient),
         'uuid': str(uuid.uuid4())
     })
