a48bcf9c3dca797ee99cb0246716e1bcb901cf99
Kajetan Rzepecki <k@spartan.works>
2017-10-31 20:52:03 +0100

    Added local currency exchange computation for non-local partners.

--- src/invoice.py
+++ src/invoice.py
@@ -9,6 +9,7 @@ import uuid
 
 
 def compute_amounts(data):
+    rate = data['exchange_rate']
     for item in data['items']:
         net_amount = item['net_price'] * item['quantity']
         vat_amount = net_amount * item['vat']/100.0
@@ -16,19 +17,31 @@ def compute_amounts(data):
         item['net_amount'] = net_amount
         item['vat_amount'] = vat_amount
         item['amount'] = amount
+        item['net_price_local'] = item['net_price'] * rate
+        item['net_amount_local'] = net_amount * rate
+        item['vat_amount_local'] = vat_amount * rate
+        item['amount_local'] = amount * rate
     return data
 
 def compute_totals(data):
     data['total_amount'] = sum([i['amount'] for i in data['items']])
     data['total_net_amount'] = sum([i['net_amount'] for i in data['items']])
     data['total_vat_amount'] = sum([i['vat_amount'] for i in data['items']])
+    rate = data['exchange_rate']
+    data['total_amount_local'] = data['total_amount'] * rate
+    data['total_net_amount_local'] = data['total_net_amount'] * rate
+    data['total_vat_amount_local'] = data['total_vat_amount'] * rate
     return data
 
 def compute_tax_summary(data):
     data['taxes'] = []
     for l in reversed(list(set(sorted([i['vat'] for i in data['items']])))):
         items = [i for i in data['items'] if i['vat'] == l]
-        data['taxes'].append(compute_totals({'vat': l, 'items': items}))
+        data['taxes'].append(compute_totals({
+            'vat': l,
+            'items': items,
+            'exchange_rate': data['exchange_rate']
+        }))
     return data
 
 def maybe_stringify(key, value):
@@ -61,17 +74,21 @@ if __name__ == "__main__":
     d = datetime.datetime.now()
     date = date_to_str(d)
     due = date_to_str(d + datetime.timedelta(days=14))
+    exchange = date_to_str(d - datetime.timedelta(days=1))
 
     p = argparse.ArgumentParser()
+    p.add_argument('--currency', '-c', help='sets the currency of the invoice', default='PLN')
+    p.add_argument('--currency-local', help='sets the local currency of the invoice', default='PLN')
     p.add_argument('--date-of-sale', '-s', help='date of sale', default=date)
     p.add_argument('--due-date', help='payment due date', default=due)
+    p.add_argument('--exchange-rate', '-e', help="exchange rate of the currencies", default="1.0")
+    p.add_argument('--exchange-rate-date', help="date of the exchange rate", default=exchange)
     p.add_argument('--issuer', '-i', help='names the issuer of the invoice')
     p.add_argument('--issue-date', '-d', help='invoice issue date', default=date)
     p.add_argument('--number', '-n', help='consecutive number of the invoice')
     p.add_argument('--number-format', '-f', help='pystache format of the invoice number', default='{{year}}/{{month}}/{{number}}')
     p.add_argument('--template', '-t', help='selects invoice template')
     p.add_argument('--recipient', '-r', help='names the recipient of the invoice')
-    p.add_argument('--currency', '-c', help='sets the currency of the invoice', default='PLN')
     p.add_argument('--uuid', '-u', help='a UUID for internal use', default=str(uuid.uuid4()))
     p.add_argument('ITEMS', nargs='+', help='invoice items')
 
@@ -79,8 +96,11 @@ if __name__ == "__main__":
 
     data = {
         'currency': args.currency,
+        'currency_local': args.currency_local,
         'date_of_sale': args.date_of_sale,
         'due_date': args.due_date,
+        'exchange_rate': float(args.exchange_rate),
+        'exchange_rate_date': args.exchange_rate_date,
         'invoice_template': args.template,
         'issuer': json.loads(args.issuer),
         'issue_date': args.issue_date,
